7ff838eb98e912ab857568cf0cf7225c8a7a1c4a
Jeonghwan Lee <brianlee6484@gmail.com>
2023-09-28 00:49:12 +0900

    feat: post mapstruct for converting dto to entitiy and vice versa added

---
+++ src/main/java/com/team/unithonbackend/domain/post/dto/PostMapper.java
@@ -0,0 +1,12 @@
+package com.team.unithonbackend.domain.post.dto;
+
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PostMapper {
+//    List<PostDto.ImageUrlResponseDto>
+
+//    List<ChatRoomDto.ChatMessageResponse> messageToMessageResponeDto(List<ChatMessage> chatMessageList);
+}
